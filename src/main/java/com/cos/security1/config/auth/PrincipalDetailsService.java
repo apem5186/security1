0b9539a4405e55c2899aa42067385a1240d0b5a8
apem5186 <apem5186@naver.com>
2024-12-26 22:59:22 +0900

    spring security 기본

---
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,34 @@
+package com.cos.security1.config.auth;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 시큐리티 session(내부 Authentication(내부 UserDetails))
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity != null) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null) {
+                SecurityContextHolder.clearContext();
+                System.out.println("Security Session cleared.");
+            }
+            System.out.println(userEntity);
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,78 @@
+package com.cos.security1.controller;
+
+import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.Iterator;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principal) {
+        System.out.println("Principal : " + principal);
+        Iterator<? extends GrantedAuthority> iterator = principal.getAuthorities().iterator();
+        while (iterator.hasNext()) {
+            GrantedAuthority authority = iterator.next();
+            System.out.println(authority.getAuthority());
+        }
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")   // SecurityConfig 파일 생성 후 작동안함
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "info";
+    }
+}
